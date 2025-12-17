15d24c55357059cc4af0610bb01e83a4d61857e0
caz <49217331+cazmura@users.noreply.github.com>
2025-12-17 23:47:04 +0900

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+number = 1
+
+while number <= 100:
+    if number % 15 == 0:
+        print('FizzBuzz')
+    elif number % 5 == 0:
+        print('Buzz')
+    elif number % 3 == 0:
+        print('Fizz')
+    else:
+        print(number)
+
+    number = number + 1
