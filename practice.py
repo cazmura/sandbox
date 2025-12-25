164cfc38ef45b153b5c4b4471f7ece1f65091a3f
caz <49217331+cazmura@users.noreply.github.com>
2025-12-25 23:33:38 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,1 +1,30 @@
-print("1\n2\n3\n")
+'''
+print("C:\test")
+
+name = "Python"
+print(f"Hello,{name}!")
+
+hoge = len("Python")
+print(hoge)
+
+x = "Python"
+print(x[0:1])
+
+x = None
+print(x)
+
+a = 10
+b = 3.14
+c = "Hello, Python!"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+lst = [1, 2, 3]
+print(lst, lst[0])
+'''
+
+
