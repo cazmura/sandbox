757a1931898c59d804d442cbc877d653330cdd15
caz <49217331+cazmura@users.noreply.github.com>
2025-12-27 22:19:43 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -25,6 +25,65 @@ print(type(d))
 
 lst = [1, 2, 3]
 print(lst, lst[0])
+
+print("C:\\test")
+
+x = "Python"
+
+print(x[0:2])
+
+x = None
+print(x)
+
+a = 10
+b = 3.14
+c = "Hello, Python!"
+d = True
+
+print(type(a))
+
+lst = [1, 2, 3]
+
+print(lst, lst[0])
+
+lst = [1, 2, 3]
+lst.append(4)
+print(lst)
+
+lst = [1, 2, 3]
+lst.remove(2)
+print(lst)
+
+lst = [1, 2]
+lst2 = [3, 4]
+
+print(lst + lst2)
+
+x = 15
+if x > 10:
+    print("xは10より大きい")
+
+x = 0
+
+if x > 10:
+    print("xは10より大きい")
+elif x > 5:
+    print("xは5より大きいが、10以下")
+else:
+    print("xは5以下")
 '''
+
+x = 20
+y = 20
+
+print(x>y)
+print(x>=y)
+print(x==y)
+print(x<=y)
+print(x<y)
+
+
+
+
 
 
